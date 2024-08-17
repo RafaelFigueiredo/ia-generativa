a428b5825fb13bb4768f6849a7bfb5b4b8e389eb
Rafael Figueiredo <rfigueiredo@garoa.cloud>
2024-08-16 21:21:10 -0300

    chore: reorganize code

--- app/main.py
+++ app/main.py
@@ -64,6 +64,7 @@ if prompt := st.chat_input("What is up?"):
 
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        response = st.write_stream(api.ask_file(prompt=prompt))[0]
+        # response = st.write_stream(intro_messages)
+        response = st.write_stream(api.ask_file(prompt=prompt))
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 run:
-	poetry run streamlit run main.py
+	poetry run streamlit run app/main.py
 
 lint:
 	poetry run black . && poetry run isort --profile=black --float-to-top . 
