c703de7fe7e634573aa6c814012daf859daa93da
Rafael Figueiredo <rfigueiredo@garoa.cloud>
2024-08-16 23:15:36 -0300

    feat: implement vector search using duckdb index

--- app/settings.py
+++ app/settings.py
@@ -8,3 +8,10 @@ EMBEDDING_BATCH_SIZE = 1000
 EMBEDDING_MODEL = "text-embedding-3-small"
 GPT_MODEL = "gpt-4o-mini"
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
+
+#https://platform.openai.com/docs/guides/embeddings
+EMBEDDING_SIZE = {
+    "text-embedding-3-small": 1536,
+    "text-embedding-3-large": 3072
+}[EMBEDDING_MODEL]
